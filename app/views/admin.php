96c1f4fc9224f2894879da60c4365a011fb64ed6
ashish336b <ashish336b@gmail.com>
2020-11-05 21:34:33 +0545

    ready to deploy

--- app/views/admin.php
+++
@@ -1,3 +0,0 @@
-<?php $this->extend("layout_1", "partials/layout"); ?>
-<h1>Body <?php echo $ok ?></h1>
-<?php $this->end() ?>
--- app/views/partials/layout.php
+++
@@ -1,3 +0,0 @@
-<?php $this->include('partials/header'); ?>
-{{layout_1}}
-<?php $this->include("partials/footer") ?>
